64c670d15c1833c2f045bdd045c6bd968c2dd3dd
Melissa Rivas <m.rivascofre@gmail.com>
2025-02-12 17:34:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
 const Amigos = [];
 const listaAmigos = document.getElementById('listaAmigos');
 const amigoResultado = document.getElementById('resultado');
